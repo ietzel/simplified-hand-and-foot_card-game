457e50e2d93052931e632003ce99357da3a60647
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-18 18:44:05 -0400

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,51 @@ func (number) goDown() {
 }
 
 func main() {
-  
+  package main
+import ("fmt")
+
+func (number) goDown() {  
+    fmt.Printf("Player ", number, "has gone down!")
+}
+
+func main() {
+  var cards = [14]int{2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+  var names = [14]string{"2","3","4","5","6","7","8","9","10","Jk","Q","K","A","Jr"}
+  var values = [14]int{20,-400,5,5,5,5,10,10,10,10,10,10,20,50}
+
+  var p1t = [64]int{}
+  var p1h = [16]int{}
+  var p1f = [16]int{}
+  var p1p int = 0
+
+  var p2t = [64]int{}
+  var p2h = [16]int{}
+  var p2f = [16]int{}
+  var p2p int = 0
+
+  var p3t = [64]int{}
+  var p3h = [16]int{}
+  var p3f = [16]int{}
+  var p3p int = 0
+
+  var p4t = [64]int{}
+  var p4h = [16]int{}
+  var p4f = [16]int{}
+  var p4p int = 0
+
+  var p5t = [64]int{}
+  var p5h = [16]int{}
+  var p5f = [16]int{}
+  var p5p int = 0
+
+  var p6t = [64]int{}
+  var p6h = [16]int{}
+  var p6f = [16]int{}
+  var p6p int = 0
+
+  fmt.Println("Get 5 books and play all your cards to go out; the player with the most points after everyone has gone out wins.")
+}
+
 
   fmt.Println("Get 5 books and play all your cards to go out; the player with the most points after everyone has gone out wins.")
 }
