6d629737c93697b4dd73b293baf739e77db399ad
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-18 18:36:59 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,6 @@
 package main
 import ("fmt")
 
-type Player struct {
-  var number int
-  var table = []int{}
-  var hand = []int{}
-  var foot = []int{}
-  var points int
-}
-
 func (p Player) goDown() {  
     fmt.Printf("Player ", p.number, "has gone down!")
 }
@@ -18,48 +10,35 @@ func main() {
   var names = [14]string{"2","3","4","5","6","7","8","9","10","Jk","Q","K","A","Jr"}
   var values = [14]int{20,-400,5,5,5,5,10,10,10,10,10,10,20,50}
 
-  var p1 Player
-  var p2 Player
-  var p3 Player
-  var p4 Player
-  var p5 Player
-  var p6 Player
+  var p1t = [64]int{}
+  var p1h = [16]int{}
+  var p1f = [16]int{}
+  var p1p int = 0
 
-  p1.number = 1
-  p1.table = []int{}
-  p1.hand = []int{}
-  p1.foot = []int{}
-  p1.points = 0
+  var p2t = [64]int{}
+  var p2h = [16]int{}
+  var p2f = [16]int{}
+  var p2p int = 0
 
-  p1.number = 2
-  p1.table = []int{}
-  p1.hand = []int{}
-  p1.foot = []int{}
-  p1.points = 0
+  var p3t = [64]int{}
+  var p3h = [16]int{}
+  var p3f = [16]int{}
+  var p3p int = 0
 
-  p1.number = 3
-  p1.table = []int{}
-  p1.hand = []int{}
-  p1.foot = []int{}
-  p1.points = 0
+  var p4t = [64]int{}
+  var p4h = [16]int{}
+  var p4f = [16]int{}
+  var p4p int = 0
 
-  p1.number = 4
-  p1.table = []int{}
-  p1.hand = []int{}
-  p1.foot = []int{}
-  p1.points = 0
+  var p5t = [64]int{}
+  var p5h = [16]int{}
+  var p5f = [16]int{}
+  var p5p int = 0
 
-  p1.number = 5
-  p1.table = []int{}
-  p1.hand = []int{}
-  p1.foot = []int{}
-  p1.points = 0
+  var p6t = [64]int{}
+  var p6h = [16]int{}
+  var p6f = [16]int{}
+  var p6p int = 0
 
-  p1.number = 6
-  p1.table = []int{}
-  p1.hand = []int{}
-  p1.foot = []int{}
-  p1.points = 0
-  
   fmt.Println("Get 5 books and play all your cards to 'go out' - the player with the most points after everyone has gone out wins.")
 }
