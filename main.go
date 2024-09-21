7f97dd39897e6644e31b0083cdb4cfe617115279
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-21 12:00:53 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,42 +2,13 @@ package main
 import ("fmt")
 
 func () round() {
-    if(p1h.length == 0) {
-        p1f = append(p1f, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    } else {
-        p1h = append(p1h, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
+
+}
+
+function (array []int) goOut {
+    for i := 0; i < len(array); i++ {
+        fmt.Println(array[i])
     }
-    turn(1)
-    if(p2h.length == 0) {
-        p2f = append(p2f, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    } else {
-        p2h = append(p2h, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    }
-    turn(2)
-    if(p3h.length == 0) {
-        p3f = append(p3f, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    } else {
-        p3h = append(p3h, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    }
-    turn(3)
-    if(p4h.length == 0) {
-        p4f = append(p4f, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    } else {
-        p4h = append(p4h, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    }
-    turn(4)
-    if(p5h.length == 0) {
-        p5f = append(p5f, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    } else {
-        p5h = append(p5h, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    }
-    turn(5)
-    if(p6h.length == 0) {
-        p6f = append(p6f, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    } else {
-        p6h = append(p6h, RandomIntegerwithinRange := rand.Intn(15-2) + 2)
-    }
-    turn(6)
 }
 
 func (number) turn {
@@ -46,38 +17,27 @@ func (number) turn {
 }
 
 func main() {
+  fmt.Printf("Less than 7 of a type of card is considered a hand book, otherwise a foot book.")
   var cards = [14]int{2,3,4,5,6,7,8,9,10,11,12,13,14,15}
   var names = [14]string{"2","3","4","5","6","7","8","9","10","Jk","Q","K","A","Jr"}
   var values = [14]int{20,-400,5,5,5,5,10,10,10,10,10,10,20,50}
 
-  var p1t = [64]int{}
-  var p1h = [32]int{}
-  var p1f = [32]int{}
-  var p1p int = 0
+  var cardsA [14]int
+  var cardsB [14]int
+  var cardsC [14]int
+  var cardsD [14]int
+  var cardsE [14]int
+  var cardsF [14]int
+  var points [6]int
 
-  var p2t = [64]int{}
-  var p2h = [32]int{}
-  var p2f = [32]int{}
-  var p2p int = 0
+  for j := 0; j <= 31; j++ {
+    cardsA[RandomIntegerwithinRange := rand.Intn(13)]++
+    cardsB[RandomIntegerwithinRange := rand.Intn(13)]++
+    cardsC[RandomIntegerwithinRange := rand.Intn(13)]++
+    cardsD[RandomIntegerwithinRange := rand.Intn(13)]++
+    cardsE[RandomIntegerwithinRange := rand.Intn(13)]++
+    cardsF[RandomIntegerwithinRange := rand.Intn(13)]++
+  }
 
-  var p3t = [64]int{}
-  var p3h = [32]int{}
-  var p3f = [32]int{}
-  var p3p int = 0
-
-  var p4t = [64]int{}
-  var p4h = [32]int{}
-  var p4f = [32]int{}
-  var p4p int = 0
-
-  var p5t = [64]int{}
-  var p5h = [32]int{}
-  var p5f = [32]int{}
-  var p5p int = 0
-
-  var p6t = [64]int{}
-  var p6h = [32]int{}
-  var p6f = [32]int{}
-  var p6p int = 0
-
+  
 }
