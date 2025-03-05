40d26f91dab05e7d3108e29ae4de489e00709a18
ietzel <67884277+ietzel@users.noreply.github.com>
2025-03-04 19:18:35 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 import ("fmt" "runtime")
 
-func () round() {
-
-}
-
 function (array []int) goOut {
     for i := 0; i < len(array); i++ {
         fmt.Println(array[i])
@@ -12,26 +8,45 @@ function (array []int) goOut {
 }
 
 func (number) turn {
+    int outingplayer = 0;
     switch num := number; num {
 	case 1:
 		cardsA = cardsA.append(RandomIntegerwithinRange := rand.Intn(13));
+	        if((cardsA).len <= 2) {
+		    outingplayer = 1;
+		}
 	case 2:
 		cardsB = cardsB.append(RandomIntegerwithinRange := rand.Intn(13));
+	        if((cardsA).len <= 2) {
+		    outingplayer = 2;
+		}
     	case 3:
 		cardsC = cardsC.append(RandomIntegerwithinRange := rand.Intn(13));
+	        if((cardsA).len <= 2) {
+		    outingplayer = 3;
+		}
     	case 4:
 		cardsD = cardsD.append(RandomIntegerwithinRange := rand.Intn(13));
+	        if((cardsA).len <= 2) {
+		    outingplayer = 4;
+		}
 	case 5:
 		cardsE = cardsE.append(RandomIntegerwithinRange := rand.Intn(13));
+	        if((cardsA).len <= 2) {
+		    outingplayer = 5;
+		}
     	case 6:
 		cardsF = cardsF.append(RandomIntegerwithinRange := rand.Intn(13));
+	        if((cardsA).len <= 2) {
+		    outingplayer = 6;
+		}
 	default:
 		fmt.Printf("No player has decided.");
     }
 }
 
 func main() {
-  fmt.Printf("Less than 7 of a type of card is considered a hand book, otherwise a foot book.")
+  fmt.Printf("7 of a type of card is considered a hand book, otherwise a foot book.");
   var cards = [14]int{2,3,4,5,6,7,8,9,10,11,12,13,14,15}
   var names = [14]string{"2","3","4","5","6","7","8","9","10","Jk","Q","K","A","Jr"}
   var values = [14]int{20,-400,5,5,5,5,10,10,10,10,10,10,20,50}
@@ -58,7 +73,7 @@ func main() {
     for i := 1; i <= 6; i++ {
     	turn(i);
     }
-    fmt.Println("turn ", turn);
+    fmt.Println("round ", turn);
     turn++
   } 
 }
